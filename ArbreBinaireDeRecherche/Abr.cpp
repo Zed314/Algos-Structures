ebfec6e59f4bb2f1d1e9310d7046ee295f143184
ziggy.vergne <ziggy.vergne.insa-lyon.fr>
2016-11-13 01:26:25 +0100

    Ajout de la méthode pour Retirer un élément de l'ABR Ajout du destructeur

--- ArbreBinaireDeRecherche/Abr.cpp
+++ ArbreBinaireDeRecherche/Abr.cpp
@@ -3,6 +3,90 @@
 #include "File.h"
 using namespace std;
 
+bool Abr::RetirerElement(Arbre * elementARetirer,Arbre * pereElementARetirer)
+{
+	if(pereElementARetirer==nullptr)
+	{
+		elementARetirer=this->racine;
+		
+	}
+	if(elementARetirer!=nullptr)
+	{
+		if(elementARetirer->gauche==nullptr&&elementARetirer->droite==nullptr)
+		{
+			
+			if(pereElementARetirer!=nullptr)
+			{
+				if(pereElementARetirer->gauche==elementARetirer)
+				{
+					pereElementARetirer->gauche=nullptr;
+				}
+				else
+				{
+					pereElementARetirer->droite=nullptr;
+				}
+			}
+			else
+			{
+				this->racine=nullptr;
+			}
+			delete elementARetirer;
+			
+		}
+		else if(elementARetirer->gauche==nullptr)
+		{
+			//On greffe le droite
+			Arbre * tempDroite=elementARetirer->droite;
+			elementARetirer->val=elementARetirer->droite->val;
+			elementARetirer->gauche=elementARetirer->droite->gauche;
+			elementARetirer->droite=elementARetirer->droite->droite;
+			delete tempDroite;
+			
+		}
+		else if(elementARetirer->droite==nullptr)
+		{
+			Arbre * tempGauche=elementARetirer->gauche;
+			elementARetirer->val=elementARetirer->gauche->val;
+			elementARetirer->gauche=elementARetirer->gauche->gauche;
+			elementARetirer->droite=elementARetirer->gauche->droite;
+			delete tempGauche;
+		}
+		else
+		{
+				//On écrase avec le plus a droite de celui à gauche
+				Arbre * elementEnCours=elementARetirer->gauche;
+				Arbre * precedent=elementARetirer;
+				while(elementEnCours->droite!=nullptr)
+				{
+					precedent=elementEnCours;
+					elementEnCours=elementEnCours->droite;
+				}
+				if(precedent==elementARetirer)
+				{
+							Arbre * tempGauche=elementARetirer->gauche;
+							elementARetirer->val=elementARetirer->gauche->val;
+							elementARetirer->gauche=elementARetirer->gauche->gauche;
+							delete tempGauche;
+				}
+				else
+				{
+					
+					elementARetirer->val=elementEnCours->val;
+					precedent->droite=elementEnCours->gauche;
+					delete elementEnCours;
+					
+				}
+				
+		}
+	}
+	else
+	{
+		return false;
+	//	cout<<"L'arbre est vide! Il est impossible de retirer des elements."<<endl;
+	}
+	return true;
+}
+
 void Abr::AffichageLargeur(Arbre * depart)
 {
 	if(depart==nullptr)
@@ -95,7 +179,11 @@ Abr::Abr()
 }
 Abr::~Abr()
 {
-	//Todo
+	while(RetirerElement())
+	{
+		//bloc vide
+	}
+	
 }
 Abr::Abr(const Abr &ArbreACopier)
 {
--- ArbreBinaireDeRecherche/Abr.h
+++ ArbreBinaireDeRecherche/Abr.h
@@ -7,11 +7,16 @@ typedef struct arbre
 	int val;
 	arbre * droite;
 	arbre * gauche;
-}Arbre;
+} Arbre;
 
 class Abr
 {
 	public:
+	bool RetirerElement(Arbre * elementARetirer=nullptr,Arbre * pereElementARetirer=nullptr);
+	//Retire l'élément pointé de l'arbre binaire de recherche
+	//Si le père est inconnu, on retire la racine
+	//Si rien n'est spécifié, on retire la racine
+	//Si l'arbre est vide, renvoie false et true sinon
 	void AffichageLargeur(Arbre * depart=nullptr);
 	bool AjouterVal(int valAAjouter);
 	//Ajoute un element si il n'est pas déjà dans l'arbre
